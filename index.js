3a5305b2511791a757210ab9aa423eff45aec1ea
Afraaz Ali <afraaz.ali@digitalconvergence.ca>
2025-07-04 21:33:34 -0700

    feat: allow limiting scenarios based on directory/path

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const getSitemap = async (domain, www) => {
     return await sitemap.fetch(`https://${www ? 'www.' : ''}${domain}/sitemap.xml`)
 }
 
-const createConfig = async (domain, reference = null) => {
+const createConfig = async (domain, reference = null, directoryLimit = null) => {
     const scenarios = []
     let res = await getSitemap(domain)
     if (!res.sites.length) {
@@ -30,17 +30,41 @@ const createConfig = async (domain, reference = null) => {
     }
     if (res.sites.length) {
         console.log(`Sitemap ${domain} ok, found ${res.sites.length} pages. Creating config`, domain)
+        // Memoization object to track directory counts
+        const directoryCounts = {}
         for (const key in res.sites) {
             if (res.sites.hasOwnProperty(key)) {
                 const url = res.sites[key];
-                scenarios.push({
-                    ...{
-                        label: url.replace(domain, '').replace(/https:|www.|http:|\/\//g, ''), // Strip url except path to get page name ;)
-                        url: url,
-                        referenceUrl: reference ? `${reference}${url.replace(domain, '').replace(/https:|www.|http:|\/\//g, '')}` : "",
-                    },
-                    ...scenario
-                })
+                let newScenario = {
+                    label: url.replace(domain, '').replace(/https:|www.|http:|\/\//g, ''),
+                    url: url,
+                    referenceUrl: reference ? `${reference}${url.replace(domain, '').replace(/https:|www.|http:|\/\//g, '')}` : "",
+                }
+                if (directoryLimit) {
+                    // Extract directory path (everything before the last segment)
+                    const urlObj = new URL(url);
+                    const pathSegments = urlObj.pathname.split('/').filter(segment => segment);
+                    const directory = pathSegments.length > 1 ? `/${pathSegments.slice(0, -1).join('/')}` : '/';
+                    // Initialize count for directory if not exists
+                    if (!directoryCounts[directory]) {
+                        directoryCounts[directory] = 0;
+                    }
+
+                    // Only push scenario if under limit of directoryLimit per directory
+                    if (directoryCounts[directory] < directoryLimit) {
+                        scenarios.push({
+                            ...newScenario,
+                            ...scenario
+                        })
+                        directoryCounts[directory]++;
+                    }
+                }
+                else {
+                    scenarios.push({
+                        ...newScenario,
+                        ...scenario
+                    })
+                }
             }
         }
         const id = domain.replace(/\//g, "-")
@@ -57,7 +81,9 @@ const createConfig = async (domain, reference = null) => {
             function (err) {
                 if (err) console.error(err)
             })
-        console.log(`Succesfully generated config for ${res.sites.length} pages on`, domain)
+        if (directoryLimit) {
+            console.log(`Succesfully generated config for ${scenarios.length} pages on`, domain)
+        }
         console.log("run: npm run setup", id)
         console.log("to generate a reference report")
     } else {
@@ -73,6 +99,7 @@ program
     .argument('<url>', 'Set url')
     .option('-d, --domains <lang,lang>', 'Set of domains extentions to check ex; de,com')
     .option('-r, --reference <url>', 'Reference url')
+    .option('-l, --directory-limit <int>', 'Limit the number of pages tested per directory')
 
 program.parse()
 
@@ -80,19 +107,25 @@ const options = program.opts();
 
 const site = program.args[0];
 const reference = options.reference ?? null;
+const directoryLimit = options.directoryLimit ?? null;
+
+if (directoryLimit && directoryLimit <= 0) {
+    throw new Error('--directory-limit should be greater than 0')
+    process.exit(1)
+}
 
 if (options.domains) {
     console.log("Creating config for", site)
     program.domains = program.domains.split(",")
-    createConfig(program.site, reference)
+    createConfig(program.site, reference, directoryLimit)
     for (const key in program.domains) {
         if (options.domains.hasOwnProperty(key)) {
             const extension = options.domains[key].indexOf('.') ? ('.' + options.domains[key]) : options.domains[key]; // if input is .de,.com instead of de,com,es for example; add a dot
             const domain = site.substring(0, site.indexOf('.')) // Strip domain extention
             createConfig(domain + extension, reference)
+            createConfig(domain, reference, directoryLimit)
         }
     }
 } else {
-    createConfig(site, reference)
+    createConfig(site, reference, directoryLimit)
 }
-
